8a71098da1c86bd7df7d00ef9bf20031488936b7
Sreenath Sreekrishna <94300241+SreenathSreekrishna@users.noreply.github.com>
2021-11-29 17:36:54 +0530

    Update __init__.py

--- encryptdecryptcrypt/__init__.py
+++ encryptdecryptcrypt/__init__.py
@@ -1,4 +1,1 @@
-from encryptdecryptcrypt import encrypt
-from encryptdecryptcrypt import decrypt
-from encryptdecryptcrypt import encryptfromfile
-from encryptdecryptcrypt import decryptfromfile
+from . import encryptdecryptcrypt
